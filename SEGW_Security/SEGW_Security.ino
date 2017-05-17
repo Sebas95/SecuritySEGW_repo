ff2a4796a313847e560ce3f434e876fe62388321
geova-25 <gjosue253@gmail.com>
2017-05-17 15:56:10 -0600

    Test 1 sensors

--- SEGW_Security/SEGW_Security.ino
+++ SEGW_Security/SEGW_Security.ino
@@ -2,13 +2,15 @@
 
 // This constant won't change:
 
-const int  inSensorSonido = 2;    // the pin that the pushbutton is attached to
-const int  inSensorPersonas = 2;  // the pin that the pushbutton is attached to
-const int  inSensorInfrarojo = 2; // the pin that the pushbutton is attached to
-const int  inBluetooth = 2;       // the pin that the pushbutton is attached to
+const int  inSensorSonido = 3;    // the pin that the sound sensor is attached to
+const int  inSensorPersonas = 5;  // the pin that the PIR sensor is attached to
+const int  inSensorInfrarojo = A5; // the pin that the infrared sensor is attached to
+const int  inBluetooth = 3;       // the pin that the bluetooth is attached to
+const int  outLaser = 2;       // the pin that the bluetooth is attached to
 
 int estadoInSonido = 0;
-
+int estadoInInfrarojo = 0;
+int estadoOutLaser = 0;
 
 
 
@@ -21,6 +23,8 @@ void setup() {
   pinMode(inSensorInfrarojo, INPUT);
   // initialize the button pin as a input:
   pinMode(inBluetooth, INPUT);
+  // initialize the button pin as a input:
+  pinMode(outLaser, OUTPUT);  
   // initialize serial communication:
   Serial.begin(9600);
 }
@@ -29,14 +33,17 @@ void setup() {
 void loop() {
   // read the pushbutton input pin:
   estadoInSonido = digitalRead(inSensorSonido);
+  estadoInInfrarojo = analogRead(inSensorInfrarojo);
+  //Serial.println(estadoInInfrarojo);
+  
+  Serial.println(estadoInSonido);
+  
+  if(estadoInInfrarojo > 130){
+    digitalWrite(outLaser,HIGH);
+  }
+  else{
+    digitalWrite(outLaser,LOW);
+  }
+  delay(50);
   
 }
-
-
-
-
-
-
-
-
-
