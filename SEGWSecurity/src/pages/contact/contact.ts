e56e9dfe09e71b2c910aaa4b2e440c303f97e19e
geova-25 <gjosue253@gmail.com>
2017-05-20 14:18:17 -0600

    All layout ready

--- SEGWSecurity/src/pages/contact/contact.ts
+++ SEGWSecurity/src/pages/contact/contact.ts
@@ -1,14 +1,33 @@
-import { Component } from '@angular/core';
+import { Component,  OnInit } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { BluetoothSerial } from '@ionic-native/bluetooth-serial'
 
 @Component({
   selector: 'page-contact',
   templateUrl: 'contact.html'
 })
-export class ContactPage {
 
-  constructor(public navCtrl: NavController) {
+
+export class ContactPage implements OnInit{
+
+   ngOnInit(){
+    alert("yeah perro")
+
+   }
+
+  private mov = 0;
+  private test = 0;
+
+  constructor(public navCtrl: NavController, public bluetoothSerial: BluetoothSerial) {
 
   }
 
+  sendMovement(){
+    this.test = this.test + 1;
+    this.bluetoothSerial.write(this.mov);
+  }
+
+  iniciarMovimientoManual(){
+    this.bluetoothSerial.write("Mm");
+  }
 }
