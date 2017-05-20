9f9f495fbb6c94048fdb334c056ce7c447a8b57d
Sebas95 <sgonzalezquesada1@gmail.com>
2017-05-20 15:32:38 -0600

    instructions reeceived from android functionality

--- SEGW_Security/SEGW_Security.ino
+++ SEGW_Security/SEGW_Security.ino
@@ -1,5 +1,6 @@
 
 #include <Servo.h> 
+#include <SoftwareSerial.h>
 // This constant won't change:
 
 
@@ -16,10 +17,15 @@ const int  outLaser = 2;       // the pin that the bluetooth is attached to
 //-------------------------------GLOBAL VARIABLES ---------------------------------------
 
 Servo myservo;  // create servo object to control a servo 
-boolean soundDetectionEnable = true;  //flag that indicates if sound detection is enabled  //################################################
+boolean soundDetectionEnable = true;  //flag that indicates if sound detection is enabled 
 int servo_position = 0;    // variable to store the servo position 
+boolean DeviceOn = false; 
+boolean manualMode = false; 
+String message; // message of bluetooth
+SoftwareSerial serial (0, 1);   //RX y TX
+  
 
-boolean DeviceOn = true;  //################################################
+
 
 void setup() {
   // initialize the button pin as a input:
@@ -35,37 +41,80 @@ void setup() {
   // initialize serial communication:
   myservo.attach(9);  // attaches the servo on pin 9 to the servo object 
   Serial.begin(9600);
+  serial.begin(9600); //comunication 
 }
 
 
 void loop() {
-  
+  receiveInstructionsFromAndroid();
   //refresh servo initial position
-  int servo_position = 0;
+  //int servo_position = 0;
   
   //if bluetooth pairing solicitude is detected (interruption)
-  if(requestForManualControl())
+  if(manualMode)
   {
     ProcessManualMovement();
   }
   //if device is on and alert detected(interruption) 
-  if(alertDetected() && DeviceOn){ 
+  if(alertDetected() && DeviceOn && !manualMode){ 
     //start automatic mode
     ProcessAutaticMovement(); 
   }
    
 }
 
-boolean requestForManualControl()  //################################################
-{
-  return false;
-}
+
 
 void ProcessManualMovement()  //################################################
 {
   
 }
 
+void receiveInstructionsFromAndroid()
+{
+  //Serial.flush(); //send data to android
+   while (serial.available()) 
+   {    // Mientras que lleguen caracteres por el puerto serial
+     delay(5);
+     char c = serial.read();     // Lee los caracteres uno a uno en la variable c
+     message += c;                 // Almacena la suma de caracteres en el mensaje  
+    
+   }  
+      if (manualMode)
+      {
+        if (message.toInt()>= 1 &&  message.toInt()<= 180 )
+        {
+           myservo.write(message.toInt());
+           Serial.println(message.toInt());
+        }
+      }
+      if ( message == "D")    {
+        DeviceOn=true;
+        Serial.println(message);}
+       
+      if( message == "d") {   
+         DeviceOn=false;
+         Serial.println(message);}
+       
+      if( message == "S"){    
+        soundDetectionEnable = true;
+        Serial.println(message);}
+      
+      if( message == "s")   {
+        soundDetectionEnable = false;
+        Serial.println(message);}
+        
+      if( message == "MM")  {
+         manualMode = true;
+         Serial.println(message);}
+      
+      if( message == "mm")    {
+         manualMode = false;
+         myservo.write(0);
+         Serial.println(message);}
+      
+    message="";
+}
 
 
 //start checking around for intruders, moving servo 3 times 180 degrees
@@ -73,24 +122,22 @@ void ProcessAutaticMovement()
 { 
     giroIzquierdo();
     giroDerecho();
-    
-     giroIzquierdo();
+    giroIzquierdo();
     giroDerecho();
-    
-     giroIzquierdo();
+    giroIzquierdo();
     giroDerecho();
 }
 
 //returns true if motion is detected
 boolean motionDetected()
 {
-  return digitalRead(inSensorPersonas) == 1;
+  return digitalRead(inSensorPersonas) == 1 && DeviceOn ;
 }
 
 //returns true if sound is detected
 boolean soundDetected()
 {
-  return digitalRead(inSensorSonido) == 1;
+  return digitalRead(inSensorSonido) == 1 && DeviceOn ;
 }
 
 //returns if an alert is happening
@@ -98,11 +145,11 @@ boolean alertDetected()
 {
   if(soundDetectionEnable) //if sound detection is enabled
   {
-    return  motionDetected() || soundDetected()  ; //checks both sound and motion sensor states
+    return  ( motionDetected() || soundDetected() )  && DeviceOn;  //checks both sound and motion sensor states
   }
   else
   { 
-    return motionDetected(); //checks only motion sensor state
+    return motionDetected() && DeviceOn;  //checks only motion sensor state
   }
 }
 
